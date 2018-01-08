cac85ee6eeb349b5dbdb55fd64455182dc869f70
DESKTOP-MUK0P1S\Mirko <mirko.eberlein@webgate.biz>
2018-01-08 21:05:58 +0100

    changes

--- src/main/java/beans/OrderAndExtrasBean.java
+++ src/main/java/beans/OrderAndExtrasBean.java
@@ -1,14 +1,20 @@
 package beans;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import constants.Sites;
+import pojos.Extra;
+import pojos.Pizza;
 
 @ManagedBean
 @SessionScoped
 public class OrderAndExtrasBean extends AbstractBeanFunctions {
+	private List<Extra> pizzaList;
+	
 	@ManagedProperty(value="#{applicationBean}")
 	private ApplicationBean appBean;
 
