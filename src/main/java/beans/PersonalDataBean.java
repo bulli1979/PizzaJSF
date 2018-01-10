0d1e2d674df399a54eb7dcbab05533c9dde3a472
Fabricio Ruch <ruch_fabricio@hotmail.com>
2018-01-10 23:30:17 +0100

    Jetzt kommts langsam gut

--- src/main/java/beans/PersonalDataBean.java
+++ src/main/java/beans/PersonalDataBean.java
@@ -11,36 +11,73 @@ import constants.Sites;
 @RequestScoped
 public class PersonalDataBean {
 	private HtmlInputText firstName;
-	@ManagedProperty(value="#{applicationBean}")
+	private HtmlInputText name;
+	private HtmlInputText street;
+	private HtmlInputText telephone;
+	private HtmlInputText zip;
+	private HtmlInputText city;
+
+	@ManagedProperty(value = "#{applicationBean}")
 	private ApplicationBean appBean;
 
 	public ApplicationBean getAppBean() {
 		return appBean;
 	}
 
-	
+	public HtmlInputText getCity() {
+		return city;
+	}
 
-	public void nextStep(){
+	public HtmlInputText getFirstName() {
+		return firstName;
+	}
+
+	public HtmlInputText getName() {
+		return name;
+	}
+
+	public HtmlInputText getStreet() {
+		return street;
+	}
+
+	public HtmlInputText getTelephone() {
+		return telephone;
+	}
+
+	public HtmlInputText getZip() {
+		return zip;
+	}
+
+	public void nextStep() {
 		appBean.setSite(Sites.OVERVIEW);
-		System.out.println(this.firstName.getValue());
-		System.out.println(appBean.getOrderData().getFirstName());
 	}
 
 	public void setAppBean(ApplicationBean appBean) {
 		this.appBean = appBean;
 	}
 
-
-
-	public HtmlInputText getFirstName() {
-		return firstName;
+	public void setCity(HtmlInputText city) {
+		this.city = city;
 	}
-
-
 
 	public void setFirstName(HtmlInputText firstName) {
 		this.firstName = firstName;
-	}	
+	}
 
+	public void setName(HtmlInputText name) {
+		this.name = name;
+	}
+
+	public void setStreet(HtmlInputText street) {
+		this.street = street;
+	}
+
+	public void setTelephone(HtmlInputText telephone) {
+		this.telephone = telephone;
+	}
+
+	public void setZip(HtmlInputText zip) {
+		this.zip = zip;
+	}
 
 }
